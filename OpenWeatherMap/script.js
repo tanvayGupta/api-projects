23b8276db93db02cd37521019b45fabea9914a93
unknown <tanvayg1@gmail.com>
2025-08-03 21:40:36 +0530

    Add basic functionality

--- OpenWeatherMap/script.js
+++ OpenWeatherMap/script.js
@@ -1,1 +1,22 @@
-const KEY = fetch("");
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
+dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const apiKey = process.env.API_KEY;
+const port = 3000;
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(port, () => {
+  console.log("Hey, I'm listening!");
+});
